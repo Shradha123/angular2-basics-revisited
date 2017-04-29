8514c72fa9cdd26d38dc4bf8b0e344d9ed6df2fb
SAPIENT\ssri36 <ssrivastav37@sapient.com>
2017-04-29 21:19:27 +0530

    changes to the app.module

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponentComponent } from './home-component/home-component.component';
 import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent,
     HomeComponent
   ],
   imports: [
